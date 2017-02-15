fa0570a09fa42cb24d1bb325e3350e56f432d110
Vicky Vishal Sahu <vicky.vishal.sahu@gmail.com>
2017-02-15 23:41:52 +0530

    Create eiengine.js

---
+++ js/eiengine.js
@@ -0,0 +1,182 @@
+/* Engine.js
+ * This file provides the game loop functionality (update entities and render),
+ * draws the initial game board on the screen, and then calls the update and
+ * render methods on your player and enemy objects (defined in your app.js).
+ *
+ * A game engine works by drawing the entire game screen over and over, kind of
+ * like a flipbook you may have created as a kid. When your player moves across
+ * the screen, it may look like just that image/character is moving or being
+ * drawn but that is not the case. What's really happening is the entire "scene"
+ * is being drawn over and over, presenting the illusion of animation.
+ *
+ * This engine is available globally via the Engine variable and it also makes
+ * the canvas' context (ctx) object globally available to make writing app.js
+ * a little simpler to work with.
+ */
+var Engine = (function(global) {
+    /* Predefine the variables we'll be using within this scope,
+     * create the canvas element, grab the 2D context for that canvas
+     * set the canvas elements height/width and add it to the DOM.
+     */
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        lastTime;
+
+    canvas.width = 640;
+    canvas.height = 384;
+    doc.body.appendChild(canvas);
+	
+	var background = new Image();
+	background.src = "images/background.jpg";
+	
+	background.onload = function(){
+		ctx.drawImage(background,0,0);
+	}
+	
+	canvas.style.border = "1px solid black";
+
+    /* This function serves as the kickoff point for the game loop itself
+     * and handles properly calling the update and render methods.
+     */
+    function main() {
+        /* Get our time delta information which is required if your game
+         * requires smooth animation. Because everyone's computer processes
+         * instructions at different speeds we need a constant value that
+         * would be the same for everyone (regardless of how fast their
+         * computer is) - hurray time!
+         */
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
+        /* Call our update/render functions, pass along the time delta to
+         * our update function since it may be used for smooth animation.
+         */
+
+        // Check to see if the game is either over or won
+        if (game.gameOver) {
+            // Player lost, render the ending image
+            renderEnd();
+        } else if (game.gameWin) {
+            // Player won, render the winning image
+            renderWin();
+        } else {
+            // Game is still going, update & render the entities
+            update(dt);
+            render();
+        }
+
+        /* Set our lastTime variable which is used to determine the time delta
+         * for the next time this function is called.
+         */
+        lastTime = now;
+
+        /* Use the browser's requestAnimationFrame function to call this
+         * function again as soon as the browser is able to draw another frame.
+         */
+        win.requestAnimationFrame(main);
+    }
+
+    /* This function does some initial setup that should only occur once,
+     * particularly setting the lastTime variable that is required for the
+     * game loop.
+     */
+    function init() {
+        lastTime = Date.now();
+        main();
+    }
+
+    /* This function is called by main (our game loop) and itself calls all
+     * of the functions which may need to update entity's data. Based on how
+     * you implement your collision detection (when two entities occupy the
+     * same space, for instance when your character should die), you may find
+     * the need to add an additional function call here. For now, we've left
+     * it commented out - you may or may not want to implement this
+     * functionality this way (you could just implement collision detection
+     * on the entities themselves within your app.js file).
+     */
+    function update(dt) {
+        updateEntities(dt);
+        // checkCollisions();
+    }
+
+    /* This is called by the update function  and loops through all of the
+     * objects within your allEnemies array as defined in app.js and calls
+     * their update() methods. It will then call the update function for your
+     * player object. These update methods should focus purely on updating
+     * the data/properties related to  the object. Do your drawing in your
+     * render methods.
+     */
+    function updateEntities(dt){
+        cannonmuzzle.update(dt);
+		target.update(dt);
+		lasers.forEach(function(laser) {
+            laser.update(dt);
+		});
+    }
+
+    /* This function initially draws the "game level", it will then call
+     * the renderEntities function. Remember, this function is called every
+     * game tick (or loop of the game engine) because that's how games work -
+     * they are flipbooks creating the illusion of animation but in reality
+     * they are just drawing the entire screen over and over.
+     */
+    function render() {
+        renderEntities();
+	}
+
+    function renderEnd() {
+        // Render the game over image
+        endImage = 'images/game-over.svg';
+        ctx.drawImage(Resources.get(endImage), 0, 0);
+    }
+	
+
+    function renderWin() {
+        // Render the game winning image
+        winImage = 'images/win-game.svg';
+		ctx.drawImage(Resources.get(winImage), 0, 0);
+	}
+
+    /* This function is called by the render function and is called on each game
+     * tick. It's purpose is to then call the render functions you have defined
+     * on your enemy and player entities within app.js
+     */
+    function renderEntities() {
+        /* Loop through all of the objects within the allEnemies array and call
+         * the render function you have defined.
+         */
+		var Cannon = 'images/Wheel-base.png';
+		ctx.drawImage(Resources.get(Cannon), 70, 302,70,70);
+		
+		var obstructionImage = 'images/obstruction.png';
+		ctx.drawImage(Resources.get(obstructionImage), 120, 192);
+		
+		lasers.forEach(function(laser) {
+                laser.render();
+        });
+
+		target.render();
+		cannonmuzzle.render();
+		
+
+    };
+
+    Resources.load([
+        'images/game-over.svg',
+        'images/win-game.svg',
+		'images/Target-Bin.png',
+		'images/Wheel-base.png',
+		'images/muzzle.png',
+		'images/obstruction.png',
+		'images/game-over.svg',
+		'images/game-over.svg',
+    ]);
+    Resources.onReady(init);
+
+    /* Assign the canvas' context object to the global variable (the window
+     * object when run in a browser) so that developers can use it more easily
+     * from within their app.js files.
+     */
+    global.ctx = ctx;
+})(this);
